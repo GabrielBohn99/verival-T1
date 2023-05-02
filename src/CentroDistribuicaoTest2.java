ab3b85151f77d3f39489ce0a98ae964b1d04c39a
Gabriel Bohn <gabrielcbohn@gmail.com>
2023-05-01 22:54:22 -0300

    feat: parameterized tests implemented

--- src/CentroDistribuicaoTest2.java
+++ src/CentroDistribuicaoTest2.java
@@ -1,64 +1,155 @@
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
 import java.util.Collection;
-
-import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 @RunWith(Parameterized.class)
-public class CentroDistribuicaoTest {
+public class CentroDistribuicaoTest2 {
 
-    @Parameterized.Parameter(0)
-    public int tAditivo;
+  private CentroDistribuicao centroDistribuicao;
+  private CentroDistribuicao.SITUACAO expectedSituacao;
+  private int expectedtGasolina;
+  private int expectedtAditivo;
+  private int expectedtAlcool1;
+  private int expectedtAlcool2;
+  private int quantidade;
+  private CentroDistribuicao.TIPOPOSTO tipoPosto;
+  private int[] resultadoEsperado;
 
-    @Parameterized.Parameter(1)
-    public int tGasolina;
+  public CentroDistribuicaoTest(
+    CentroDistribuicao centroDistribuicao,
+    CentroDistribuicao.SITUACAO expectedSituacao,
+    int expectedtGasolina,
+    int expectedtAditivo,
+    int expectedtAlcool1,
+    int expectedtAlcool2,
+    int quantidade,
+    CentroDistribuicao.TIPOPOSTO tipoPosto,
+    int[] resultadoEsperado
+  ) {
+    this.centroDistribuicao = centroDistribuicao;
+    this.expectedSituacao = expectedSituacao;
+    this.expectedtGasolina = expectedtGasolina;
+    this.expectedtAditivo = expectedtAditivo;
+    this.expectedtAlcool1 = expectedtAlcool1;
+    this.expectedtAlcool2 = expectedtAlcool2;
+    this.quantidade = quantidade;
+    this.tipoPosto = tipoPosto;
+    this.resultadoEsperado = resultadoEsperado;
+  }
 
-    @Parameterized.Parameter(2)
-    public int tAlcool1;
+  @Parameters
+  public static Collection<Object[]> data() {
+    return Arrays.asList(
+      new Object[][] {
+        {
+          new CentroDistribuicao(500, 10000, 1000, 1000),
+          CentroDistribuicao.SITUACAO.NORMAL,
+          10000,
+          500,
+          1000,
+          1000,
+          5000,
+          CentroDistribuicao.TIPOPOSTO.COMUM,
+          new int[] { 250, 6500, 375, 375 },
+        },
+        {
+          new CentroDistribuicao(200, 3000, 500, 500),
+          CentroDistribuicao.SITUACAO.SOBRAVISO,
+          3000,
+          200,
+          500,
+          500,
+          2000,
+          CentroDistribuicao.TIPOPOSTO.ESTRATEGICO,
+          new int[] { 100, 1600, 250, 250 },
+        },
+        {
+          new CentroDistribuicao(200, 3000, 500, 500),
+          CentroDistribuicao.SITUACAO.SOBRAVISO,
+          3000,
+          200,
+          500,
+          500,
+          1000,
+          CentroDistribuicao.TIPOPOSTO.COMUM,
+          new int[] { 175, 2650, 438, 438 },
+        },
+        {
+          new CentroDistribuicao(100, 5000, 300, 300),
+          CentroDistribuicao.SITUACAO.EMERGENCIA,
+          5000,
+          100,
+          300,
+          300,
+          1000,
+          CentroDistribuicao.TIPOPOSTO.COMUM,
+          new int[] { -14, 0, 0, 0 },
+        },
+        {
+          new CentroDistribuicao(100, 5000, 300, 300),
+          CentroDistribuicao.SITUACAO.EMERGENCIA,
+          5000,
+          100,
+          300,
+          300,
+          1000,
+          CentroDistribuicao.TIPOPOSTO.ESTRATEGICO,
+          new int[] { 75, 4650, 238, 238 },
+        },
+        {
+          new CentroDistribuicao(500, 10000, 1000, 1000),
+          CentroDistribuicao.SITUACAO.NORMAL,
+          10000,
+          500,
+          1000,
+          1000,
+          0,
+          CentroDistribuicao.TIPOPOSTO.ESTRATEGICO,
+          new int[] { 500, 10000, 1000, 1000 },
+        },
+        {
+          new CentroDistribuicao(500, 10000, 1000, 1000),
+          CentroDistribuicao.SITUACAO.NORMAL,
+          10000,
+          500,
+          1000,
+          1000,
+          -100,
+          CentroDistribuicao.TIPOPOSTO.ESTRATEGICO,
+          new int[] { -7, 0, 0, 0 },
+        },
+        {
+          new CentroDistribuicao(50, 10000, 1000, 1000),
+          CentroDistribuicao.SITUACAO.EMERGENCIA,
+          10000,
+          50,
+          1000,
+          1000,
+          2000,
+          CentroDistribuicao.TIPOPOSTO.ESTRATEGICO,
+          new int[] { -21, 0, 0, 0 },
+        },
+      }
+    );
+  }
 
-    @Parameterized.Parameter(3)
-    public int tAlcool2;
-
-    @Parameterized.Parameter(4)
-    public CentroDistribuicao.SITUACAO expectedSituacao;
-
-    @Parameterized.Parameters
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][]{
-                {MAX_ADITIVO / 2, MAX_GASOLINA / 2, MAX_ALCOOL / 4, MAX_ALCOOL / 4, CentroDistribuicao.SITUACAO.NORMAL},
-                {MAX_ADITIVO / 4, MAX_GASOLINA / 4, MAX_ALCOOL / 8, MAX_ALCOOL / 8, CentroDistribuicao.SITUACAO.SOBRAVISO},
-                {MAX_ADITIVO / 8, MAX_GASOLINA / 8, MAX_ALCOOL / 16, MAX_ALCOOL / 16, CentroDistribuicao.SITUACAO.EMERGENCIA},
-                {0, MAX_GASOLINA, MAX_ALCOOL / 2, MAX_ALCOOL / 2, CentroDistribuicao.SITUACAO.EMERGENCIA},
-                {MAX_ADITIVO, MAX_GASOLINA, MAX_ALCOOL / 2, MAX_ALCOOL / 2, CentroDistribuicao.SITUACAO.EMERGENCIA},
-                {MAX_ADITIVO / 2, MAX_GASOLINA, MAX_ALCOOL / 2, MAX_ALCOOL / 2, CentroDistribuicao.SITUACAO.SOBRAVISO},
-                {MAX_ADITIVO / 2, MAX_GASOLINA / 2, 0, MAX_ALCOOL / 2, CentroDistribuicao.SITUACAO.EMERGENCIA},
-                {MAX_ADITIVO / 2, MAX_GASOLINA / 2, MAX_ALCOOL / 2, 0, CentroDistribuicao.SITUACAO.EMERGENCIA},
-                {MAX_ADITIVO / 2, MAX_GASOLINA / 2, 0, 0, CentroDistribuicao.SITUACAO.EMERGENCIA},
-                {MAX_ADITIVO / 2, MAX_GASOLINA / 2, MAX_ALCOOL / 2, MAX_ALCOOL / 2 + 1, CentroDistribuicao.SITUACAO.EMERGENCIA}
-        });
+  @Test
+  public void testAbastecimento() {
+    centroDistribuicao.abastece(quantidade, tipoPosto);
+    assertEquals(expectedSituacao, centroDistribuicao.getSituacao());
+    assertEquals(expectedtGasolina, centroDistribuicao.gettGasolina());
+    assertEquals(expectedtAditivo, centroDistribuicao.gettAditivo());
+    assertEquals(expectedtAlcool1, centroDistribuicao.gettAlcool1());
+    assertEquals(expectedtAlcool2, centroDistribuicao.gettAlcool2());
+    int[] result = centroDistribuicao.verificaEstoque();
+    assertEquals(resultadoEsperado.length, result.length);
+    for (int i = 0; i < resultadoEsperado.length; i++) {
+      assertEquals(resultadoEsperado[i], result[i]);
     }
-
-    @Test
-    public void testDefineSituacao() {
-        CentroDistribuicao centro = new CentroDistribuicao(tAditivo, tGasolina, tAlcool1, tAlcool2);
-        assertEquals(expectedSituacao, centro.getSituacao());
-    }
-
-    // caso de teste 2: encomendaCombustivel com quantidade insuficiente de gasolina
-    assertEquals(new int[] {-1,-1,-1,-1}, centro.encomendaCombustivel(500, TIPOPOSTO.COMUM));
-    
-    // caso de teste 3: encomendaCombustivel com quantidade insuficiente de aditivo
-    assertEquals(new int[] {0,0,0,-1}, centro.encomendaCombustivel(5, TIPOPOSTO.COMUM));
-    
-    // caso de teste 4: encomendaCombustivel com quantidade insuficiente de alcool
-    assertEquals(new int[] {0,0,0,-1}, centro.encomendaCombustivel(125, TIPOPOSTO.COMUM));
-    
-    // caso de teste 5: encomendaCombustivel com quantidade suficiente de combustível comum
-    assertEquals(new int[] {200, 50, 100, 0}, centro.encomendaCombustivel(350, TIPOPOSTO.COMUM));
-    
-    // caso de teste 6: encomendaCombustivel com quantidade suficiente de combustível estratégico
-    assertEquals(new int[] {1000, 200, 400, 500}, centro.encomendaCombustivel(2100, TIPOPOSTO.ESTRATEGICO));
+  }
 }
